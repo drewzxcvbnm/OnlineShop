dbc22815d310838d82346a7316e26938df8ab39a
Drewman <drewzxcvbnm0@gmail.com>
2020-11-21 17:31:38 +0200

    fix category

--- src/main/java/com/online/shop/application/mappers/CategoryMapper.java
+++ src/main/java/com/online/shop/application/mappers/CategoryMapper.java
@@ -3,6 +3,7 @@ package com.online.shop.application.mappers;
 import com.online.shop.application.dto.CategoryDto;
 import com.online.shop.application.entities.Category;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
