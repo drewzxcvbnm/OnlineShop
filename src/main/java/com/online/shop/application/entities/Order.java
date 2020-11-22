70f685e63b98e8e615ed2a7071b438b7450be3d3
andris.zacs <andris.zacs@accenture.com>
2020-11-22 13:25:55 +0200

    order status from ordinal to string

--- src/main/java/com/online/shop/application/entities/Order.java
+++ src/main/java/com/online/shop/application/entities/Order.java
@@ -24,6 +24,7 @@ public class Order {
     private String surname;
     private String address;
     private String bankAccount;
+    @Enumerated(EnumType.STRING)
     private OrderStatus status;
     @JoinColumn
     @ManyToOne(optional = true)
