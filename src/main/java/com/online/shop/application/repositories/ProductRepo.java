0acde0e0dfc3b35779264968e5dc715e8a2b56ea
Drewman <drewzxcvbnm0@gmail.com>
2020-11-22 17:53:39 +0200

    add some useless queries

--- src/main/java/com/online/shop/application/repositories/ProductRepo.java
+++ src/main/java/com/online/shop/application/repositories/ProductRepo.java
@@ -1,12 +1,23 @@
 package com.online.shop.application.repositories;
 
 import com.online.shop.application.entities.Product;
+import com.online.shop.application.entities.User;
 import com.online.shop.application.exceptions.ItemNotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ProductRepo extends JpaRepository<Product, Long> {
 
     Product getById(Long id);
+
+    @Query(value = "select u.* from PRODUCT p " +
+            "join PURCHASE P2 on p.ID = P2.PRODUCT_ID " +
+            "join \"order\" o on P2.\"order_id\" = o.ID " +
+            "join USER U on o.USER_ID = U.ID\n" +
+            "where p.ID=?1", nativeQuery = true)
+    List<User> getAllProductOwners();
 
     default Product findObligatoryProduct(long productId) {
         return findById(productId)
--- src/main/java/com/online/shop/application/repositories/UserRepo.java
+++ src/main/java/com/online/shop/application/repositories/UserRepo.java
@@ -1,8 +1,11 @@
 package com.online.shop.application.repositories;
 
+import com.online.shop.application.entities.Order;
 import com.online.shop.application.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepo extends JpaRepository<User, Long> {
@@ -12,4 +15,10 @@ public interface UserRepo extends JpaRepository<User, Long> {
     User getByUsername(String username);
 
     User findUserById(long id);
+
+    @Query(value = "SELECT o.* FROM USER u " +
+            "JOIN \"order\" o on o.user_id = u.id " +
+            "where u.id=?1",
+            nativeQuery = true)
+    List<Order> getUserOrders(long userId);
 }
